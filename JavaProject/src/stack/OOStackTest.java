2909f7014c80a6041d9068102095173de9e274c0
brunodecruz <brunodecruz.05@gmail.com>
2023-09-18 18:43:54 -0300

    test modified

--- JavaProject/src/stack/OOStackTest.java
+++ JavaProject/src/stack/OOStackTest.java
@@ -2,90 +2,60 @@ package stack;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
-
 public class OOStackTest {
-
   @Test public void test01StackShouldBeEmptyWhenCreated() {
     assertTrue( new OOStack().isEmpty() );
   }
-
   @Test public void test02PushAddElementsToTheStack() {
     assertFalse( new OOStack().push( "Something" ).isEmpty() );
   }
-
   @Test public void test03PopRemovesElementsFromTheStack() {
-    OOStack stack = new OOStack();
-    stack.push( "Something" );
+    OOStack stack = getStack( pushedObject);
     stack.pop();
     assertTrue( stack.isEmpty() );
   }
-
   @Test public void test04PopReturnsLastPushedObject() {
-    OOStack stack = new OOStack();
-    String pushedObject = "Something";
-    stack.push( pushedObject );
+    OOStack stack = getStack(pushedObject);
     assertEquals( stack.pop(), pushedObject );
   }
-
   @Test public void test05StackBehavesLIFO() {
-    OOStack stack = new OOStack();
-    String firstPushedObject = "First";
-    String secondPushedObject = "Second";
-
-    stack.push( firstPushedObject );
+    OOStack stack = getStack(firstPushedObject);
     stack.push( secondPushedObject );
-
     assertEquals( stack.pop(), secondPushedObject );
     assertEquals( stack.pop(), firstPushedObject );
     assertTrue( stack.isEmpty() );
   }
-
   @Test public void test06TopReturnsLastPushedObject() {
-    String pushedObject = "Something";
-    assertEquals( new OOStack().push( pushedObject ).top(), 
-                  pushedObject );
+    assertEquals( getStack(pushedObject).top(), pushedObject );
   }
-
   @Test public void test07TopDoesNotRemoveObjectFromStack() {
-    OOStack stack = new OOStack();
-    stack.push( "Something" );
+    OOStack stack = getStack(pushedObject);
     assertEquals( stack.size(), 1 );
     stack.top();
     assertEquals( stack.size(), 1 );
   }
-
   @Test public void test08CanNotPopWhenThereAreNoObjectsInTheStack() {
     OOStack stack = new OOStack();
-    try {
-      stack.pop();
-      fail( "Expected MessageNotUnderstood Error was not thrown." );
-    } catch (Error e) {
-      assertTrue( e.getMessage().equals( OOStack.stackEmptyErrorDescription ) );
-    }
+    assertThrows(Error.class, () -> stack.pop(),OOStack.stackEmptyErrorDescription );
   }
-
   @Test public void test09CanNotPopWhenThereAreNoObjectsInTheStackAndTheStackHadObjects() {
-    OOStack stack = new OOStack();
-    stack.push( "Something" );
+    OOStack stack = getStack(pushedObject);
     stack.pop();
-    try {
-      stack.pop();
-      fail( "Expected MessageNotUnderstood Error was not thrown." );
-    } catch (Error e) {
-      assertTrue( e.getMessage().equals( OOStack.stackEmptyErrorDescription ) );
-    }
-  }
-
+    assertThrows(Error.class, () -> stack.pop(),OOStack.stackEmptyErrorDescription );
+  } 
   @Test public void test10CanNotTopWhenThereAreNoObjectsInTheStack() {
     OOStack stack = new OOStack();
-    try {
-      stack.top();
-      fail( "Expected MessageNotUnderstood Error was not thrown." );
-    } catch (Error e) {
-      assertTrue( e.getMessage().equals( OOStack.stackEmptyErrorDescription ) );
-    }
+    assertThrows(Error.class, () -> stack.top(),OOStack.stackEmptyErrorDescription );
+  }
+  String pushedObject = "Something";
+  String firstPushedObject = "First";
+  String secondPushedObject = "Second";
+  private OOStack getStack(String elment) {
+    OOStack stack = new OOStack();
+    stack.push( elment );
+    return stack;
   }
 }
